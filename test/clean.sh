d7fabbc5259adcd5fd07f12f3dad7c30efeb287b
xingqiang.yuan <xingqiang.yuan@okg.com>
2025-09-09 16:35:07 +0800

    update

--- test/clean.sh
+++ test/clean.sh
@@ -2,3 +2,16 @@
 
 docker compose down
 rm -rf data
+
+rm -rf config-op/genesis.json
+rm -rf config-op/genesis.json.gz
+rm -rf config-op/implementations.json
+rm -rf config-op/intent.toml
+rm -rf config-op/rollup.json
+rm -rf config-op/state.json
+rm -rf config-op/superchain.json
+rm -rf l1-geth/consensus/beacondata/
+rm -rf l1-geth/consensus/genesis.ssz
+rm -rf l1-geth/consensus/validatordata/
+rm -rf l1-geth/execution/genesis.json
+rm -rf l1-geth/execution/geth/
