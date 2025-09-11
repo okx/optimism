4a7a87a2abcd26624ef5bc4dc589c977db786459
xingqiang.yuan <xingqiang.yuan@okg.com>
2025-09-11 10:30:43 +0800

    update

--- test/1-start-l1.sh
+++ test/1-start-l1.sh
@@ -9,6 +9,12 @@ sleep 3
 OP_BATCHER_ADDR=$(cast wallet a $OP_BATCHER_PRIVATE_KEY)
 OP_PROPOSER_ADDR=$(cast wallet a $OP_PROPOSER_PRIVATE_KEY)
 OP_CHALLENGER_ADDR=$(cast wallet a $OP_CHALLENGER_PRIVATE_KEY)
+
+while [[ "$(cast rpc eth_syncing)" != "false" ]]; do
+    echo "Waiting for node to finish syncing..."
+    sleep 1
+done
+
 cast send --private-key $RICH_L1_PRIVATE_KEY --value 100ether $OP_BATCHER_ADDR --legacy
 cast send --private-key $RICH_L1_PRIVATE_KEY --value 100ether $OP_PROPOSER_ADDR --legacy
 cast send --private-key $RICH_L1_PRIVATE_KEY --value 100ether $OP_CHALLENGER_ADDR --legacy
