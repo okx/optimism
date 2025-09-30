00fcbd9d5b8c14bb2aff8bad1ac64bd250691110
Niven <sieniven@gmail.com>
2025-09-30 17:36:20 +0800

    Update

--- op-node/rollup/sequencing/sequencer_xlayer.go
+++ op-node/rollup/sequencing/sequencer_xlayer.go
@@ -1,12 +1,12 @@
 package sequencing
 
 import (
-	"bytes"
 	"context"
 	"fmt"
 	"math/big"
 
 	"github.com/ethereum-optimism/optimism/op-service/eth"
+	"github.com/ethereum/go-ethereum/beacon/engine"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/log"
@@ -16,13 +16,6 @@ import (
 	"github.com/ethereum/go-ethereum/trie"
 	"github.com/holiman/uint256"
 )
-
-type rawTransactions []eth.Data
-
-func (s rawTransactions) Len() int { return len(s) }
-func (s rawTransactions) EncodeIndex(i int, w *bytes.Buffer) {
-	w.Write(s[i])
-}
 
 func (s *Sequencer) InitRealtimeXLayer() {
 	if s.rollupCfg.Realtime.SequencerEnable {
@@ -51,32 +44,14 @@ func (s *Sequencer) SendRealtimeErrorTrigger(height uint64) {
 func (s *Sequencer) SendRealtimeConfirmedBlock(envelope *eth.ExecutionPayloadEnvelope) {
 	if s.active.Load() && s.rollupCfg != nil && s.rollupCfg.Realtime != nil && s.rollupCfg.Realtime.SequencerEnable {
 		if s.realtimeBlockInfoChan != nil {
-			payload := envelope.ExecutionPayload
-			hasher := trie.NewStackTrie(nil)
-			txHash := types.DeriveSha(rawTransactions(payload.Transactions), hasher)
+			header, err := s.ExecutionPayloadToBlockHeader(envelope)
+			if err != nil {
+				log.Error("[Realtime] Failed to convert execution payload to block header", "error", err)
+				return
+			}
+			// Calculate transaction hash using the same method as geth
 			s.realtimeBlockInfoChan <- &realtimeTypes.BlockInfo{
-				Header: &types.Header{
-					ParentHash:       payload.ParentHash,
-					UncleHash:        types.EmptyUncleHash,
-					Coinbase:         payload.FeeRecipient,
-					Root:             common.Hash(payload.StateRoot),
-					TxHash:           txHash,
-					ReceiptHash:      common.Hash(payload.ReceiptsRoot),
-					Bloom:            types.Bloom(payload.LogsBloom),
-					Difficulty:       common.Big0, // zeroed, proof-of-work legacy
-					Number:           big.NewInt(int64(payload.BlockNumber)),
-					GasLimit:         uint64(payload.GasLimit),
-					GasUsed:          uint64(payload.GasUsed),
-					Time:             uint64(payload.Timestamp),
-					Extra:            payload.ExtraData,
-					MixDigest:        common.Hash(payload.PrevRandao),
-					Nonce:            types.BlockNonce{}, // zeroed, proof-of-work legacy
-					BaseFee:          (*uint256.Int)(&payload.BaseFeePerGas).ToBig(),
-					WithdrawalsHash:  nil, // set below
-					BlobGasUsed:      (*uint64)(payload.BlobGasUsed),
-					ExcessBlobGas:    (*uint64)(payload.ExcessBlobGas),
-					ParentBeaconRoot: envelope.ParentBeaconBlockRoot,
-				},
+				Header:    header,
 				TxCount:   int64(len(envelope.ExecutionPayload.Transactions)),
 				Hash:      envelope.ExecutionPayload.BlockHash,
 				Changeset: envelope.Changeset,
@@ -89,4 +64,60 @@ func (s *Sequencer) SetRealtimeEnabledXLayer(attrs *eth.PayloadAttributes) {
 	if s.active.Load() && s.rollupCfg != nil && s.rollupCfg.Realtime != nil {
 		attrs.RealtimeEnabled = s.rollupCfg.Realtime.SequencerEnable
 	}
+}
+
+func (s *Sequencer) ExecutionPayloadToBlockHeader(envelope *eth.ExecutionPayloadEnvelope) (*types.Header, error) {
+	payload := envelope.ExecutionPayload
+	// For txHash
+	txBytes := make([][]byte, len(payload.Transactions))
+	for i, tx := range payload.Transactions {
+		txBytes[i] = []byte(tx)
+	}
+	txs, err := engine.DecodeTransactionsXLayer([][]byte(txBytes))
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode transactions for realtime block: %w", err)
+	}
+	// For withdrawals hash
+	var withdrawalsRoot *common.Hash
+	if payload.WithdrawalsRoot != nil {
+		withdrawalsRoot = payload.WithdrawalsRoot
+	} else if payload.Withdrawals != nil {
+		wr := types.DeriveSha(*payload.Withdrawals, trie.NewStackTrie(nil))
+		withdrawalsRoot = &wr
+	}
+	// For requests hash
+	var requestsHash *common.Hash
+	if s.rollupCfg.NewPayloadVersion(uint64(payload.Timestamp)) == eth.NewPayloadV4 {
+		requests := [][]byte{}
+		h := types.CalcRequestsHash(requests)
+		requestsHash = &h
+	}
+
+	header := &types.Header{
+		ParentHash:       payload.ParentHash,
+		UncleHash:        types.EmptyUncleHash,
+		Coinbase:         payload.FeeRecipient,
+		Root:             common.Hash(payload.StateRoot),
+		TxHash:           types.DeriveSha(types.Transactions(txs), trie.NewStackTrie(nil)),
+		ReceiptHash:      common.Hash(payload.ReceiptsRoot),
+		Bloom:            types.Bloom(payload.LogsBloom),
+		Difficulty:       common.Big0, // zeroed, proof-of-work legacy
+		Number:           new(big.Int).SetUint64(uint64(payload.BlockNumber)),
+		GasLimit:         uint64(payload.GasLimit),
+		GasUsed:          uint64(payload.GasUsed),
+		Time:             uint64(payload.Timestamp),
+		BaseFee:          (*uint256.Int)(&payload.BaseFeePerGas).ToBig(),
+		Extra:            payload.ExtraData,
+		MixDigest:        common.Hash(payload.PrevRandao),
+		WithdrawalsHash:  withdrawalsRoot,
+		ExcessBlobGas:    (*uint64)(payload.ExcessBlobGas),
+		BlobGasUsed:      (*uint64)(payload.BlobGasUsed),
+		ParentBeaconRoot: envelope.ParentBeaconBlockRoot,
+		RequestsHash:     requestsHash,
+	}
+	if header.Hash() != payload.BlockHash {
+		return nil, fmt.Errorf("block hash mismatch: %s != %s", header.Hash(), payload.BlockHash)
+	}
+
+	return header, nil
 }
