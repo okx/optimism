3654f34716131ae996226ace019b76f0e6efbd20
jason.huang <20609724+doutv@users.noreply.github.com>
2025-10-08 18:23:20 +0800

    fix Transactor error

--- test/2-deploy-op-contracts.sh
+++ test/2-deploy-op-contracts.sh
@@ -27,7 +27,7 @@ TRANSACTOR_DEPLOY_OUTPUT=$(docker run --rm \
   forge create --json --broadcast \
     --rpc-url $L1_RPC_URL_IN_DOCKER \
     --private-key $DEPLOYER_PRIVATE_KEY \
-    src/periphery/Transactor.sol:Transactor.0.8.30 \
+    src/periphery/Transactor.sol:Transactor \
     --constructor-args $ADMIN_OWNER_ADDRESS)
 
 # Extract contract address from deployment output
