35dba124d539d17a1a7d4fd80cca0b7c251a26e1
Dumi Loghin <dumiloghin@gmail.com>
2025-10-22 11:53:46 +0800

    fix: init op-geth-rpc datadir on re-start

--- test/scripts/start-rpc.sh
+++ test/scripts/start-rpc.sh
@@ -6,4 +6,11 @@ source .env
 
 set -x
 
+if [ "$RPC_TYPE" = "op-geth-rpc" ]; then
+  OP_GETH_GENESIS_JSON="$(pwd)/config-op/genesis.json"
+  OP_GETH_RPC_DATADIR="$(pwd)/data/op-geth-rpc"
+  docker run --rm -v $OP_GETH_RPC_DATADIR:/datadir -v $OP_GETH_GENESIS_JSON:/genesis.json op-geth:latest init --datadir /datadir /genesis.json
+  sleep 3
+fi
+
 docker compose up -d op-rpc
