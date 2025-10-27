7937cd814ebc8bee45182f34f7e8f77cc183953e
lucas.lim <lucas.lim@okg.com>
2025-10-27 11:22:45 +0800

    fix: add gpo ignore flag to prevent transaction filtered out

--- test/entrypoint/reth-rpc.sh
+++ test/entrypoint/reth-rpc.sh
@@ -23,4 +23,5 @@ exec op-reth node \
       --authrpc.port=8552 \
       --authrpc.jwtsecret=/jwt.txt \
       --rollup.disable-tx-pool-gossip \
-      --rollup.sequencer-http=http://op-geth-seq:8545
+      --rollup.sequencer-http=http://op-geth-seq:8545 \
+      --gpo.ignoreprice=1
