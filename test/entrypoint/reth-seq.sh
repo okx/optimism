56cf7c0b834b653ab7d39acf00d3755d8891722c
cliff.yang <cliff.yang@okg.com>
2025-11-05 17:51:17 +0800

    update reth-seq.sh

--- test/entrypoint/reth-seq.sh
+++ test/entrypoint/reth-seq.sh
@@ -15,12 +15,12 @@ start_op_reth_node() {
           --http.corsdomain=* \
           --http.port=8545 \
           --http.addr=0.0.0.0 \
-          --http.api=web3,debug,eth,txpool,net,miner \
+          --http.api=web3,debug,eth,txpool,net,miner,admin \
           --ws \
           --ws.addr=0.0.0.0 \
           --ws.port=7546 \
           --ws.origins=* \
-          --ws.api=web3,debug,eth,txpool,net \
+          --ws.api=web3,debug,eth,txpool,net,admin \
           --disable-discovery \
           --max-outbound-peers=10 \
           --max-inbound-peers=10 \
@@ -33,6 +33,7 @@ start_op_reth_node() {
           --txpool.basefee-max-count=500000 \
           --txpool.queued-max-count=500000 \
           --txpool.max-account-slots=10000 \
+          --txpool.disable-transactions-backup \
           --log.file.directory "$log_path" \
           --log.stdout.filter "info,txpool=trace,payload_builder=debug,engine=debug,network=debug" \
           --trusted-peers=enode://ef8135659def07b48b54fe2de7d0368e3eaa0a080ef13dde560169357900954be1a1e890b5973a821f9158e512a2da3ff600368f44e18e725a86931eaae5ef64@op-${SEQ_TYPE}-seq:30303
