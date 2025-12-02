bb184682c742dacd7a0709b44f37ab289a1484f2
AgusDuha <81362284+agusduha@users.noreply.github.com>
2025-12-02 13:58:23 -0300

    feat: add CGT system feature (#18405)
    * feat: add CGT system feature (#703)
    
    * fix: sys feature CI (#706)
    
    * fix: liquidity controller fuzz (#708)
    
    * fix: setup CGT system feature in deploy script (#711)
    
    * fix: unused import (#713)
    
    * test: add CGT missing tests (#714)

--- packages/contracts-bedrock/scripts/libraries/Config.sol
+++ packages/contracts-bedrock/scripts/libraries/Config.sol
@@ -251,8 +251,8 @@ library Config {
         return vm.envOr("DEV_FEATURE__DEPLOY_V2_DISPUTE_GAMES", false);
     }
 
-    /// @notice Returns true if the development feature custom gas token is enabled.
-    function devFeatureCustomGasToken() internal view returns (bool) {
-        return vm.envOr("DEV_FEATURE__CUSTOM_GAS_TOKEN", false);
+    /// @notice Returns true if the system feature custom_gas_token is enabled.
+    function sysFeatureCustomGasToken() internal view returns (bool) {
+        return vm.envOr("SYS_FEATURE__CUSTOM_GAS_TOKEN", false);
     }
 }
